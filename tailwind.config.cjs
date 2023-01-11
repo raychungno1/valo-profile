c86100e9a0063e18454c189ea36141766ca27522
Ray Chung <raychungno1@gmail.com>
2023-01-11 11:44:42 -0500

    init astro repository and basic responsive nav-bar

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			transitionTimingFunction: {
+				"in-out-quint": "cubic-bezier(0.19, 1, 0.22, 1)"
+			}
+		},
+	},
+	plugins: [],
+}
