d1e95b5da29165347fec7b8176a55e3a86699357
Ray Chung <raychungno1@gmail.com>
2023-01-11 20:37:15 -0500

    added navbar styles

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			transitionTimingFunction: {
-				"in-out-quint": "cubic-bezier(0.19, 1, 0.22, 1)"
-			}
-		},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        highlight: "rgba(233, 237, 179)",
+      },
+      transitionTimingFunction: {
+        "in-out-quint": "cubic-bezier(0.19, 1, 0.22, 1)",
+      },
+    },
+  },
+  plugins: [],
+};
