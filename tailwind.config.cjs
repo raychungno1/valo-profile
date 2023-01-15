d9a4efcf2159b55b827d8e2742e228dde951097e
Ray Chung <raychungno1@gmail.com>
2023-01-15 18:33:48 -0500

    reformatted index page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,9 +5,14 @@ module.exports = {
     extend: {
       colors: {
         highlight: "rgba(233, 237, 179)",
+        "highlight-green": "rgba(50, 200, 160)",
       },
       transitionTimingFunction: {
         "in-out-quint": "cubic-bezier(0.19, 1, 0.22, 1)",
+      },
+      fontFamily: {
+        "custom-normal": ["Just Dance Normal", "sans-serif"],
+        "custom-heavy": ["Just Dance Bold", "sans-serif"],
       },
     },
   },
