250a6219a3448c22c15ae691662762fe997292ee
Ray Chung <raychungno1@gmail.com>
2023-01-25 20:02:55 -0500

    valo profile logic rework

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,6 @@
 import { defineConfig } from "astro/config";
-
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
-
-// https://astro.build/config
 import mdx from "@astrojs/mdx";
-
-// https://astro.build/config
 import image from "@astrojs/image";
 
 // https://astro.build/config
