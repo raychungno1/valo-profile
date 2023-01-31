8ec472ada9bf614f8c2c1d465f57bc638a49e63b
Ray Chung <raychungno1@gmail.com>
2023-01-30 23:16:26 -0500

    added awards section

--- src/utils/companies.ts
+++ src/utils/companies.ts
@@ -2,8 +2,16 @@ import bloomberg from "../images/companies/bloomberg.png";
 import chase from "../images/companies/chase.png";
 import osu from "../images/companies/osu.png";
 
+import asa from "../images/companies/asa.png";
+import pltw from "../images/companies/pltw.png";
+import bsa from "../images/companies/bsa.png";
+
 export const companies: { [name: string]: ImageMetadata } = {
   bloomberg,
   chase,
   osu,
+
+  asa,
+  pltw,
+  bsa,
 };
