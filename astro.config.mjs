07ba2fbf8001768050207312f02a79a631235637
Ray Chung <raychungno1@gmail.com>
2023-02-05 22:41:09 -0500

    updated astro site name & readme

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://raychungno1.netlify.app/",
   integrations: [
     tailwind(),
     mdx(),
